aeaa275e518881bb4ac53f839d91fee6cd2579dc
VadimGordeev <vadimgordeev22@gmail.com>
2023-03-05 00:50:04 +0300

    feat: made modal view with simple events

---
+++ script/view/ModalView/constant.js
@@ -0,0 +1,19 @@
+export const ModalNameAttr = {
+    TaskUserSelect: 'users',
+    TaskButtonCancel: 'modal_btn_cancel',
+    TaskButtonConfirm: 'modal_btn_confirm',
+}
+
+export const ModalAddInputAttr = {
+    TaskAddTitleInput: 'add_task_title_input',
+    TaskAddDescriptionInput: 'add_task_description_input',
+}
+
+export const ModalEditInputAttr = {
+    TaskEditTitleInput: 'edit_task_title_input',
+    TaskEditDescriptionInput: 'edit_task_description_input',
+}
+
+export const ListOptions = ['User1', 'User2', 'User3', 'User4'];
+
+export const ListOptionsLength = ListOptions.length;
