34e1e1c9745100976b7716fd99eebe64816080d1
Mikhail Vezhnovets <mvezhnovets@gmail.com>
2023-03-05 21:48:55 +0300

    add button & counter view

--- script/view/index.js
+++ script/view/index.js
@@ -1,9 +1,16 @@
 import { ModalAddView, ModalEditView, ModalWarningView } from "./ModalView/index.js";
+import { TaskTodoContainer, TaskInProgressContainer, TaskDoneContainer } from "./TaskContainer/index.js";
+
 export class TaskView {
     constructor () {
         this.modalAdd = new ModalAddView('modal_add');
         this.modalEdit = new ModalEditView('modal_edit');
-        this.modalWarning = new ModalWarningView ('modal_warning')
+        this.modalWarning = new ModalWarningView ('modal_warning');
+        
+        this.todoContainer = new TaskTodoContainer('card_progress_add', 'header_add')
+        this.inprogressContainer = new TaskInProgressContainer('card_progress_inprogress','header_inprogress')
+        this.doneContainer = new TaskDoneContainer('card_progress_done','header_done')
+        
     }
 
 }
