32df6e2c90de05fb9043cf4283fd6fcc5519646b
IgnatBelskiy <belskiy_ignat@mail.ru>
2023-03-08 00:10:21 +0300

    Header-Ignat

---
+++ script/view/CardView/constants.js
@@ -0,0 +1,12 @@
+export const taskContainers = {
+  add: "card_container_add",
+  inProgress: "card_container_inprogress",
+  completed: "card_container_done",
+};
+export const taskConstants = {
+  title: "h4",
+  description: "Description",
+  user: "User: ",
+  tagHeader: "h5",
+  tagInner: "p",
+};
