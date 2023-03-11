07f7980c10e8809f07d947e08d44289702a54247
VadimGordeev <vadimgordeev22@gmail.com>
2023-03-11 22:14:22 +0300

    refactoring

--- script/view/TaskContainer/utils.js
+++ script/view/TaskContainer/utils.js
@@ -1,44 +1,15 @@
 import { createButton } from "../utils.js";
 
-export function createAddButton() {
-   const addButton = createButton('Add task', 'button_add', {
-      type: 'button',
-      name: 'addTaskButton',
-   })
+export function createContainerButton (title,/*  className, */ buttonProps, buttonId) {
+   const btn = createButton (title,/*  className, */ buttonProps);
+   btn.id = buttonId;
 
-   addButton.id ='button_add';
-   addButton.classList.add('button');
-
-   return addButton
+   return btn
 }
 
-export function createInProgressButton() {
-   const inProgressButton = createButton('Done all', 'button_done_all', {
-      type: 'button',
-      name: 'doneTaskButton',
-   })
-
-   inProgressButton.id ='button_done_all';
-   inProgressButton.classList.add('button');
-
-   return inProgressButton
-}
-
-export function createDeleteAllButton() {
-   const deleteAllButton = createButton('Delete all', 'button_delete_all', {
-      type: 'button',
-      name: 'deleteTaskButton',
-   })
-
-   deleteAllButton.id ='button_delete_all';
-   deleteAllButton.classList.add('button');
-
-   return deleteAllButton
-}
-
-export function createCounter( className1, className2, id, value = 0 ) {
+export function createCounter( id, value ) {
    const counter = document.createElement('p');
-   counter.classList.add(className1, className2);
+   counter.classList.add('counter_task');
    counter.id = id;
    counter.textContent = `${value}`;
 
--- script/view/index.js
+++ script/view/index.js
@@ -9,12 +9,44 @@ export class TaskView {
     this.modalEdit = new ModalView('modal_edit', 'modal_edit_cancel', 'modal_edit_confirm', 'Task 1', 'This is Task 1 Description', 1);
     this.modalWarning = new ModalWarningView("modal_warning");
 
-    this.todoContainer = new TaskTodoContainer("card_progress_add", "header_add");
-    this.inprogressContainer = new TaskInProgressContainer(
-      "card_progress_inprogress",
-      "header_inprogress"
-    );
-    this.doneContainer = new TaskDoneContainer("card_progress_done", "header_done");
+    this.todoContainer = new TaskContainer({
+        containerId:'card_progress_add',
+        headerId:'header_add',
+        title:  'Add task',
+        buttonProps: {
+            type: 'button',
+            name: 'addTaskButton',
+         }, 
+         id: 'button_add',
+         value: 0,
+         counterId: 'counter_todo',
+    })
+
+    this.inprogressContainer = new TaskContainer({
+        containerId:'card_progress_inprogress',
+        headerId:'header_inprogress',
+        title:  'Done all',
+        buttonProps: {
+            type: 'button',
+            name: 'doneTaskButton',
+         }, 
+         id: 'button_done_all',
+         value: 0,
+         counterId: 'counter_inprogress',
+    })
+
+    this.doneContainer = new TaskContainer({
+        containerId:'card_progress_done',
+        headerId:'header_done',
+        title:  'Delete all',
+        buttonProps: {
+            type: 'button',
+            name: 'deleteTaskButton',
+         }, 
+         id: 'button_delete_all',
+         value: 0,
+         counterId: 'counter_done',
+    })
 
     this.card_add = new TaskCard("card_container_add");
     this.card_inProgress = new TaskCard("card_container_inprogress");
