6c0fb0f435f6dd283dacfe21930bf949af3ecb84
VadimGordeev <vadimgordeev22@gmail.com>
2023-03-11 22:14:22 +0300

    feat: refactor modal view

--- script/view/ModalView/utils.js
+++ script/view/ModalView/utils.js
@@ -1,15 +1,17 @@
-import { ModalNameAttr, ModalAddInputAttr, ModalEditInputAttr, ListOptions, ListOptionsLength } from './constant.js';
+import { ModalNameAttr, ModalInputAttr, ListOptions, ListOptionsLength } from './constant.js';
 import { createTextInput, createButton } from '../utils.js'; 
 
-export function createTaskAddModal () {
+export function createTaskModal (cancelId, confirmId, valueTitle, valueDescription) {
     const form = document.createElement('form');
     form.classList.add('modal');
     
-    const inputTitle = createTextInput ({ name: ModalAddInputAttr.TaskAddTitleInput, placeholder: 'Title' })
+    const inputTitle = createTextInput ({ name: ModalInputAttr.TaskTitleInput, placeholder: 'Title'})
     inputTitle.classList.add('input_title');
+    inputTitle.value = valueTitle
 
-    const inputDescription = createTextInput ({ name: ModalAddInputAttr.TaskAddDescriptionInput, placeholder: 'Description' })
+    const inputDescription = createTextInput ({ name: ModalInputAttr.TaskDescriptionInput, placeholder: 'Description' })
     inputDescription.classList.add('input_info');
+    inputDescription.value = valueDescription;
 
     const btnContainer = document.createElement('div')
     btnContainer.classList.add('modal_btn');
@@ -28,54 +30,13 @@ export function createTaskAddModal () {
         type: 'button',
         name: ModalNameAttr.TaskButtonCancel
     });
-    cancelBtn.id = 'modal_add_cancel'
+    cancelBtn.id = cancelId;
 
     const confirmBtn = createButton ('Confirm', 'modal_btn_confirm', {
         type: 'submit',
         name: ModalNameAttr.TaskButtonConfirm
     });
-    confirmBtn.id = 'modal_add_confirm';
-
-    btnContainer.append(select, cancelBtn, confirmBtn);
-    form.append(inputTitle, inputDescription, btnContainer);
-
-    return form;
-}
-
-export function createTaskEditModal () {
-    const form = document.createElement('form');
-    form.classList.add('modal');
-    
-    const inputTitle = createTextInput ({ name: ModalEditInputAttr.TaskEditTitleInput, placeholder: 'Title' })
-    inputTitle.classList.add('input_title');
-
-    const inputDescription = createTextInput ({ name: ModalEditInputAttr.TaskEditDescriptionInput, placeholder: 'Description' })
-    inputDescription.classList.add('input_info');
-
-    const btnContainer = document.createElement('div')
-    btnContainer.classList.add('modal_btn');
-
-    const select = document.createElement('select');
-    select.classList.add('modal_select');
-    select.setAttribute('name', 'users');
-    for (let i = 0; i < ListOptionsLength; i += 1) {
-        const option = document.createElement('option');
-        option.value = ListOptions[i];
-        option.text = ListOptions[i];
-        select.add(option)
-    }
-    
-    const cancelBtn = createButton ('Cancel', 'modal_btn_cancel', {
-        type: 'button',
-        name: ModalNameAttr.TaskButtonCancel
-    });
-    cancelBtn.id = 'modal_edit_cancel'
-
-    const confirmBtn = createButton ('Confirm', 'modal_btn_confirm', {
-        type: 'submit',
-        name: ModalNameAttr.TaskButtonConfirm
-    });
-    confirmBtn.id = 'modal_edit_confirm';
+    confirmBtn.id = confirmId;
 
     btnContainer.append(select, cancelBtn, confirmBtn);
     form.append(inputTitle, inputDescription, btnContainer);
