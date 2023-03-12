acbaeacc96854b3bc02956f2ee1ca4e195be4406
Mikhail Vezhnovets <mvezhnovets@gmail.com>
2023-03-12 15:23:42 +0300

    refactoring

--- script/view/TaskContainer/index.js
+++ script/view/TaskContainer/index.js
@@ -1,5 +1,62 @@
-import { createAddButton, createInProgressButton, createDeleteAllButton, createCounter } from "./utils.js"
-export class TaskTodoContainer {
+import { createContainerButton, createCounter } from "./utils.js"
+
+export class TaskGeneralContainer {
+   constructor( { containerId, headerId, title, className, buttonProps, buttonId, value, counterId } ) {
+      this.container = document.getElementById(containerId);
+      this.header = document.getElementById(headerId);
+
+      this.count = createCounter(counterId, value);
+      this.button = createContainerButton(title, className, buttonProps, buttonId);
+
+      this.container.append(this.button);
+      this.header.append(this.count);
+
+      this.container.addEventListener('click', ( { target } ) => {
+         if (target.id === 'button_delete_all') {
+            console.log("delete task");
+         } else if (target.id === 'button_done_all') {
+            console.log("done all task");
+         } else if (target.id === 'button_add') {
+            console.log("add task");
+         }
+      })
+   }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* export class TaskTodoContainer {
    constructor(containerId, headerId) {
       this.container = document.getElementById(containerId);
       this.header = document.getElementById(headerId);
@@ -61,3 +118,4 @@ export class TaskDoneContainer {
    }
 
 }
+ */
--- script/view/index.js
+++ script/view/index.js
@@ -1,6 +1,6 @@
 import { ModalView } from "./ModalView/ModalView.js";
 import { ModalWarningView } from "./ModalView/ModalWarning.js";
-import { TaskTodoContainer, TaskInProgressContainer, TaskDoneContainer } from "./TaskContainer/index.js";
+import { TaskGeneralContainer } from "./TaskContainer/index.js";
 import { TaskCard } from "./CardView/TaskCard.js";
 
 export class TaskView {
@@ -9,12 +9,47 @@ export class TaskView {
     this.modalEdit = new ModalView('modal_edit', 'modal_edit_cancel', 'modal_edit_confirm', 'Task 1', 'This is Task 1 Description', 1);
     this.modalWarning = new ModalWarningView("modal_warning");
 
-    this.todoContainer = new TaskTodoContainer("card_progress_add", "header_add");
-    this.inprogressContainer = new TaskInProgressContainer(
-      "card_progress_inprogress",
-      "header_inprogress"
-    );
-    this.doneContainer = new TaskDoneContainer("card_progress_done", "header_done");
+    this.todoContainer = new TaskGeneralContainer({
+        containerId:'card_progress_add',
+        headerId:'header_add',
+        title:  'Add task',
+/*         className: 'button_add', */ 
+        buttonProps: {
+            type: 'button',
+            name: 'addTaskButton',
+         }, 
+         id: 'button_add',
+         value: 0 /* statusCount.todo */,
+         counterId: 'counter_todo',
+    })
+
+    this.inprogressContainer = new TaskGeneralContainer({
+        containerId:'card_progress_inprogress',
+        headerId:'header_inprogress',
+        title:  'Done all',
+/*         className: 'button_done_all',  */
+        buttonProps: {
+            type: 'button',
+            name: 'doneTaskButton',
+         }, 
+         id: 'button_done_all',
+         value: 0 /* statusCount.inProgress */,
+         counterId: 'counter_inprogress',
+    })
+
+    this.doneContainer = new TaskGeneralContainer({
+        containerId:'card_progress_done',
+        headerId:'header_done',
+        title:  'Delete all',
+/*         className: 'button_delete_all',  */
+        buttonProps: {
+            type: 'button',
+            name: 'deleteTaskButton',
+         }, 
+         id: 'button_delete_all',
+         value: 0 /* statusCount.done */,
+         counterId: 'counter_done',
+    })
 
     this.card_add = new TaskCard("card_container_add");
     this.card_inProgress = new TaskCard("card_container_inprogress");
